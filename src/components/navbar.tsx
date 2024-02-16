05c2afde99c6330f4d04694ed328cb9af21ff1db
marcowang01 <marcowang01@gmail.com>
2024-02-16 00:50:22 -0500

    prettier

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,37 +1,22 @@
-import { NavbarItem } from "./navbarItem"
-import { HomeIcon } from "@/assets/home-icon"
-import { CmdIcon } from "@/assets/cmd-icon"
-import { GearIcon } from "@/assets/gear-icon"
-import { SearchBar } from "./searchBar"
+import { NavbarItem } from "./navbarItem";
+import { HomeIcon } from "@/assets/home-icon";
+import { CmdIcon } from "@/assets/cmd-icon";
+import { GearIcon } from "@/assets/gear-icon";
+import { SearchBar } from "./searchBar";
 
 export function Navbar() {
-
   const searchBar = () => {
-    return (
-      <input>
-
-      </input>
-    )
-  }
+    return <input></input>;
+  };
 
   return (
     <div className="flex flex-row gap-3">
-      <NavbarItem
-        icon={<HomeIcon />}
-      />
+      <NavbarItem icon={<HomeIcon />} />
       <SearchBar />
-      <NavbarItem
-        text={"newsletter"}
-      />
-       <NavbarItem
-        text={"apartment"}
-      />
-       <NavbarItem
-        text={"job"}
-      />
-       <NavbarItem
-        text={"friends"}
-      />
+      <NavbarItem text={"newsletter"} />
+      <NavbarItem text={"apartment"} />
+      <NavbarItem text={"job"} />
+      <NavbarItem text={"friends"} />
     </div>
-  )
+  );
 }
