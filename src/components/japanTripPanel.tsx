cf2c57b576865bdf8d47e3221f128d62e46ed329
marcowang01 <marcowang01@gmail.com>
2024-02-21 08:58:22 -0600

    added more japan bnaseline

--- src/components/japanTripPanel.tsx
+++ src/components/japanTripPanel.tsx
@@ -1,7 +1,13 @@
 export function JapanTripPanel() {
   return (
-    <div>
-      <h1>Japan Trip</h1>
+    <div className="w-full h-full flex flex-col">
+      <div className="text-3xl font-light">
+        Bookings for your April Trip to Japan
+      </div>
+      <div className="w-full h-[1px] bg-gray-350" />
+      <div>
+        
+      </div>
       <p>
         This is the Japan trip panel. You can add any content here.
       </p>
