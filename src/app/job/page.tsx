b34450a2493013540cec7425d305d08427036a52
marcowang01 <marcowang01@gmail.com>
2024-03-07 19:56:07 -0600

    add new content for all pages

--- src/app/job/page.tsx
+++ src/app/job/page.tsx
@@ -1,8 +1,34 @@
+import { jobEmails } from "@/data/categories";
+import { EmailRow } from "@/components/home/emailRow";
+import { ReactNode } from "react";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Job</h1>
-      <p>Job page content</p>
-    </div>
+    <Wrapper>
+      {jobEmails.map((email, index) => {
+        return (
+          <EmailRow
+            key={`summary-${index}`}
+            email={email}
+            sender={email.sender}
+            title={email.title}
+            gapClass={"gap-[50px]"}
+            actions={["view", "edit"]}
+          />
+        );
+      })}
+    </Wrapper>
   );
+}
+
+export function Wrapper({children}: {children: ReactNode}) {
+  return (
+  <div className="w-full h-full">
+      <h1 className="text-[30px] text-gray-500 font-light">Work and opportunities</h1>
+      <div className="w-full h-[1px] bg-gray-350 my-[20px]"/>
+      <div className="flex flex-col w-full mt-[10px] h-[calc(100vh-300px)] pr-[5px] overflow-y-auto">
+        {children}
+      </div>
+    </div>
+  )
 }
